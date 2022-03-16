ac45064df3224fab1215329cd8a1414327634372
Fernando Benavides <fernandousb17@gmail.com>
2022-03-16 14:25:51 -0500

    Init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const inputFileRef = useRef(null)
+
+  const [image, setImage] = useState({name:'', url:''})
+
+  const handleClickRef = () => {
+    inputFileRef.current.click()
+  }
+
+  const handleInputFile = (e) =>{
+    if(e.target.files){
+      setImage({
+        name: e.target.files[0].name,
+        url: URL.createObjectURL(e.target.files[0])
+      })
+    }
+  }
+
+  return(
+    <>
+      <div className="container p-5">
+        <h1 className="text-center">React Input File</h1>
+        <div className="my-3 inputFile d-flex justify-content-center align-items-center" onClick={handleClickRef}>
+          <p className="m-0 fw-bolder">¡Click to upload a file!</p>
+        </div>
+        <input onChange={handleInputFile} type='file' id='file' ref={inputFileRef} accept="image/png, image/gif, image/jpeg"  style={{display: 'none'}}/>
+
+        {image.url && 
+        <div className="text-center">
+          <p><span className="fw-bold">Uploaded file: </span>{image.name}</p>
+          <div className="mw-100"><img className="img-fluid" src={image.url} /></div>
+        </div>
+        }
+      </div>
+    </>
+  )
 }
 
 export default App;
